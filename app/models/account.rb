585b3ba18342329f5104732e98a0c9f7f753d796
Thomas Phillips <ThomasPhillips1024@outlook.com>
2021-12-01 00:49:48 +0530

    fix account spec issue

--- app/models/account.rb
+++ app/models/account.rb
@@ -92,7 +92,7 @@ class Account < ApplicationRecord
   validates :username, format: { with: /\A[a-z0-9_]+\z/i }, length: { maximum: 30 }, if: -> { local? && will_save_change_to_username? && actor_type != 'Application' }
   validates_with UnreservedUsernameValidator, if: -> { local? && will_save_change_to_username? }
   validates :display_name, length: { maximum: 30 }, if: -> { local? && will_save_change_to_display_name? }
-  validates :note, length: { minimum: StaticSetting.last&.min_profile_description_character || 0, maximum: 500 }, if: -> { local? && will_save_change_to_note? }
+  validates :note, note_length: { maximum: 500 }, length: { minimum: StaticSetting.last&.min_profile_description_character || 0}, if: -> { local? && will_save_change_to_note? }
   validates :fields, length: { maximum: 4 }, if: -> { local? && will_save_change_to_fields? }
 
   scope :remote, -> { where.not(domain: nil) }

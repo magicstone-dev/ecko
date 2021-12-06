836723d0232d27c15f5796f2e21e7ddadae94c1e
David Sterry <davesterry@gmail.com>
2021-12-05 19:09:09 -0800

    change sanity check to 5k chars

--- app/models/static_setting.rb
+++ app/models/static_setting.rb
@@ -12,7 +12,7 @@
 #  updated_at                        :datetime         not null
 #
 class StaticSetting < ApplicationRecord
-	validates :max_post_character, numericality: { greater_than: 0, less_than_or_equal_to: 1000}, unless: -> { max_post_character.blank? }
+	validates :max_post_character, numericality: { greater_than: 0, less_than_or_equal_to: 5000}, unless: -> { max_post_character.blank? }
 	validates :max_poll_options, numericality: { greater_than: 0, less_than_or_equal_to: 10}, unless: -> { max_poll_options.blank? }
 	validates :max_poll_option_character, numericality: { greater_than: 0, less_than_or_equal_to: 1000}, unless: -> { max_poll_option_character.blank? }
 	validates :user_fields, numericality: { greater_than: 0, less_than_or_equal_to: 10}, unless: -> { user_fields.blank? }

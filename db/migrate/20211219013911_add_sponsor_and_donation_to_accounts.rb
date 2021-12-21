68adac6367e3f0da2d4e0bde892761a369cd985f
David Sterry <davesterry@gmail.com>
2021-12-21 14:18:23 -0800

    change default after add as suggested by migration process

--- db/migrate/20211219013911_add_sponsor_and_donation_to_accounts.rb
+++ db/migrate/20211219013911_add_sponsor_and_donation_to_accounts.rb
@@ -1,6 +1,8 @@
 class AddSponsorAndDonationToAccounts < ActiveRecord::Migration[6.1]
   def change
-    add_column :accounts, :sponsor, :integer, default: 0
-    add_column :accounts, :donation_amount, :float, default: 0
+    add_column :accounts, :sponsor, :integer
+    change_column_default :accounts, :sponsor, 0
+    add_column :accounts, :donation_amount, :float
+    change_column_default :accounts, :donation_amount, 0
   end
 end

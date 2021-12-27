c7f251004ac130a0489194ae169491bd4e1a589d
David Sterry <davesterry@gmail.com>
2021-12-27 10:52:55 -0800

    update wording, add more usage info

--- lib/mastodon/donations_cli.rb
+++ lib/mastodon/donations_cli.rb
@@ -13,37 +13,37 @@ module Mastodon
       true
     end
 
-    desc 'donation packages', 'Create donation packages'
+    desc 'create_packages', 'Create donation packages'
     long_desc <<-LONG_DESC
-      Creates donation packages which will be used to refer for donations
+      Creates default donation packages of $2, $10, and $50
     LONG_DESC
     def create_packages
       DonationPackage.destroy_all
       packages = [
         {
           id: 1,
-          amount: 10,
+          amount: 2,
           currency: 0,
           title: 'Silver',
-          description: 'Silver Packages',
+          description: 'Silver Level',
           donation_reference: 'silver_tier',
           visible: true,
         },
         {
           id: 2,
-          amount: 100,
+          amount: 10,
           currency: 0,
           title: 'Gold',
-          description: 'Gold Packages',
+          description: 'Gold Level',
           donation_reference: 'gold_tier',
           visible: true,
         },
         {
           id: 3,
-          amount: 200,
+          amount: 50,
           currency: 0,
           title: 'Platinum',
-          description: 'Platinum Packages',
+          description: 'Platinum Level',
           donation_reference: 'platinum_tier',
           visible: true,
         },
@@ -52,15 +52,21 @@ module Mastodon
         DonationPackage.create(package)
       end
 
-      say("#{packages.length} Donation Packages created", :green)
+      say("#{packages.length} donation packages created", :green)
     end
 
     option :currency, type: :string, default: 'usd', aliases: [:c]
     option :visible, type: :boolean, default: true, aliases: [:v]
     option :reference, type: :string, default: 'silver_tier', aliases: [:r]
-    desc 'donation packages', 'Create donation packages'
+    desc 'create_package <title> <amount> <description> [OPTIONS]', 'Create donation package'
     long_desc <<-LONG_DESC
-      Creates donation package which will be used to refer for donations
+      Creates donation package based on supplied arguments.
+        <title>             name shown on donate page
+        <amount>            in whole dollars or euros
+        <description>       descriptive text shown on donate page
+        -c, --currency      usd or eur (default: usd)
+        -v, --visible       visible on donate page (default: true)
+        -r, --reference     silver_tier, gold_tier or platinum_tier for badge color
     LONG_DESC
     def create_package(title, amount, description)
       DonationPackage.create(
@@ -72,22 +78,23 @@ module Mastodon
         visible: options[:visible]
       )
 
-      say("#{title} donation Package created", :green)
+      say("#{title} donation package created", :green)
     end
 
 
-    desc 'donation packages', 'Create donation packages'
+    desc 'remove_package <title>', 'Remove donation package by title'
     long_desc <<-LONG_DESC
-      Removes donation package which already exists
+      Removes donation package from the database.
+        <title>             name of package on donate page
     LONG_DESC
     def remove_package(title)
       package = DonationPackage.find_by(title: title)
 
       if package.nil?
-        say("Donation Package with title #{title} doesnt exists", :red)
+        say("Donation package with title #{title} doesn't exist", :red)
       else
         package.destroy!
-        say("#{title} donation Package has been removed", :green)
+        say("#{title} donation package has been removed", :green)
       end
     end
   end

3a56bc7def7fcaadad5350dbe143d257ca544fb8
Manish Sharma <arnoltherasing@gmail.com>
2022-01-19 13:56:28 +0600

    revert poll option change (#377)

--- app/serializers/rest/poll_serializer.rb
+++ app/serializers/rest/poll_serializer.rb
@@ -4,10 +4,7 @@ class REST::PollSerializer < ActiveModel::Serializer
   attributes :id, :expires_at, :expired,
              :multiple, :votes_count, :voters_count
 
-  has_many :loaded_options, key: :options do
-    object.loaded_options.shuffle
-  end
-
+  has_many :loaded_options, key: :options
   has_many :emojis, serializer: REST::CustomEmojiSerializer
 
   attribute :voted, if: :current_user?
